3d41af561989043c38f3c217c1981f83a411a05d
shahadaste <52976770+shahadaste@users.noreply.github.com>
2019-07-16 15:39:59 -0400

    add files via upload

---
+++ IAMHUMAN.js
@@ -0,0 +1,13 @@
+function toggleSection(id) {
+
+var elt = document.getElementById(id);
+
+  if (elt.style.visibility === 'hidden') {
+    elt.style.visibility = 'visible';
+  } else {
+    elt.style.visibility = 'hidden';
+  }
+
+
+
+}
